be554628e8ee6e6af6948951eb9dc4802ced12b7
VadimBykovMisis <146062569+VadimBykovMisis@users.noreply.github.com>
2024-04-13 16:12:02 +0300

    Lab7 upload

---
+++ lab7/lab7_1.cs
@@ -0,0 +1,134 @@
+using System;
+public class Program
+{
+    public class SurveyParticipant
+    {
+        private string answer = "";
+        public SurveyParticipant(string answer)
+        {
+            this.answer = answer;
+        }
+        public string result
+        {
+            get
+            {
+                return this.answer;
+            }
+        }
+    }
+    public abstract class Survey
+    {
+        private List<SurveyParticipant> p = new List<SurveyParticipant>();
+        public void AddAnswer(string answer)
+        {
+            SurveyParticipant a = new SurveyParticipant(answer);
+            this.p.Add(a);
+        }
+        private (List<string>, List<int>) GetUniqueAnswers()
+        {
+            List<string> uniqueAnswers = new List<string>();
+            List<int> uniqueAnswersCount = new List<int>();
+            for (int i = 0; i < this.p.Count; i++)
+            {
+                int isUnique = 1;
+                for (int j = 0; j < uniqueAnswers.Count; j++)
+                {
+                    if (this.p[i].result == uniqueAnswers[j])
+                    {
+                        isUnique = 0;
+                        uniqueAnswersCount[j] += 1;
+                    }
+                }
+                if (isUnique == 1)
+                {
+                    uniqueAnswers.Add(this.p[i].result);
+                    uniqueAnswersCount.Add(1);
+                }
+            }
+            return (uniqueAnswers, uniqueAnswersCount);
+        }
+        private (List<string>, List<int>) SortUniqueAnswers(List<string> uniqueAnswers, List<int> uniqueAnswersCount)
+        {
+            for (int i = 0; i < uniqueAnswers.Count; i++)
+            {
+                for (int j = i + 1; j < uniqueAnswers.Count; j++)
+                {
+                    if (uniqueAnswersCount[i] < uniqueAnswersCount[j])
+                    {
+                        string answerBuffer = uniqueAnswers[j];
+                        int answerCountBuffer = uniqueAnswersCount[j];
+                        uniqueAnswers[j] = uniqueAnswers[i];
+                        uniqueAnswersCount[j] = uniqueAnswersCount[i];
+                        uniqueAnswers[i] = answerBuffer;
+                        uniqueAnswersCount[i] = answerCountBuffer;
+                    }
+                }
+            }
+            return (uniqueAnswers, uniqueAnswersCount);
+        }
+        protected void ShowResult()
+        {
+            var uniqueAnswers = this.GetUniqueAnswers();
+            uniqueAnswers = this.SortUniqueAnswers(uniqueAnswers.Item1, uniqueAnswers.Item2);
+            Console.WriteLine(String.Format("{0,10} {1,10}", "Ответ:", "Доля: "));
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(String.Format("{0,10} {1,10}", uniqueAnswers.Item1[i], (double)(uniqueAnswers.Item2[i]) / this.p.Count));
+            }
+        }
+        public abstract void RunSurvey();
+    }
+    public class ManOfTheYear : Survey
+    {
+        public override void RunSurvey()
+        {
+            Console.WriteLine("Результаты опроса 'Человек года: '");
+            this.ShowResult();
+        }
+    }
+    public class OpeningOfTheYear : Survey
+    {
+        public override void RunSurvey()
+        {
+            Console.WriteLine("Результаты опроса 'Открытие года: '");
+            this.ShowResult();
+        }
+    }
+        public static void Main(string[] args)
+    {
+        Survey a = new ManOfTheYear();
+        a.AddAnswer("Быков");
+        a.AddAnswer("Быков");
+        a.AddAnswer("Быков");
+        a.AddAnswer("Быков");
+        a.AddAnswer("Быков");
+        a.AddAnswer("Круглов");
+        a.AddAnswer("Круглов");
+        a.AddAnswer("Илон Маск");
+        a.AddAnswer("Илон Маск");
+        a.AddAnswer("Илон Маск");
+        a.AddAnswer("Илон Маск");
+        a.AddAnswer("Илон Маск");
+        a.AddAnswer("Крутов");
+        a.AddAnswer("Крутов");
+        a.AddAnswer("Крутов");
+        a.AddAnswer("Крутов");
+        a.AddAnswer("Харитонов");
+        a.RunSurvey();
+        Survey b = new OpeningOfTheYear();
+        b.AddAnswer("Данил");
+        b.AddAnswer("Данил");
+        b.AddAnswer("Данил");
+        b.AddAnswer("Данил");
+        b.AddAnswer("Данил");
+        b.AddAnswer("Данил");
+        b.AddAnswer("Кирилл");
+        b.AddAnswer("Петр");
+        b.AddAnswer("Петр");
+        b.AddAnswer("Петр");
+        b.AddAnswer("Женя");
+        b.AddAnswer("Женя");
+        b.AddAnswer("Александр");
+        b.RunSurvey();
+    }
+}
